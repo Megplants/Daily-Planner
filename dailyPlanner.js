09fa1fbe2e5264d54ff689ba9d298116a38c40ea
Megplants <119674430+Megplants@users.noreply.github.com>
2024-08-03 14:22:24 -0400

    Add files via upload

---
+++ dailyPlanner.js
@@ -0,0 +1,25 @@
+
+<><script language="javascript" type="text/javascript">
+
+    dayName = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+    monName = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
+
+    now = new Date
+
+
+</script><style>
+        .material-symbols-outlined {font - variation - settings}:
+        'FILL' 1,
+        'wght' 400,
+        'GRAD' 0,
+        'opsz' 24
+        }
+    </style></>
+
+const priorityIcon = ["star", "star_half", ]
+   
+function priority {
+
+    }
+
+    
